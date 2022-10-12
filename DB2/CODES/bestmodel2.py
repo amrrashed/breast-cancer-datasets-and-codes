fcbd4557058f43d9f6b879fad1fdcbec765e1f31
Amr Ez Eldin Elsayed Rashed <amrrashed200@gmail.com>
2022-10-12 23:21:04 +0300

    Add files via upload

---
+++ DB2/CODES/bestmodel2.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import OneHotEncoder, StackingEstimator
+from xgboost import XGBClassifier
+from tpot.export_utils import set_param_recursive
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=1)
+
+# Average CV score on the training set was: 0.7888447971781305
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=GaussianNB()),
+    StackingEstimator(estimator=XGBClassifier(learning_rate=0.01, max_depth=4, min_child_weight=20, n_estimators=100, n_jobs=1, subsample=0.35000000000000003, verbosity=0)),
+    OneHotEncoder(minimum_fraction=0.2, sparse=False, threshold=10),
+    RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.9500000000000001, min_samples_leaf=13, min_samples_split=16, n_estimators=100)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 1)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
